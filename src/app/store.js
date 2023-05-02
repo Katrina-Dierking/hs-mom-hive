aa9e754cfb8fd84930111b5d0107a154b645ee4f
Katrina <kalynndier@gmail.com>
2023-05-02 12:52:06 -0400

    resource created

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import resourcesReducer from '../features/resources/resourcesSlice'
 
 export const store =configureStore({
     reducer: {
-        
+        resource:resourcesReducer,
     }
 })
---
+++ src/features/resources/Resources.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useSelector, useDispatch} from 'react-redux'
+import { ordered, restocked } from './resourcesSlice'
+
+const Resources = () => {
+
+ const numOfResources = useSelector(state => state.resource.numOfResources)
+ const dispatch = useDispatch()
+
+  return (
+    <div>
+        <h2>Number of Resources - {numOfResources}</h2>
+        <button onClick={()=> dispatch(ordered())}>Order Resource</button>
+        <button onClick = {() => dispatch(restocked(2))}>Restock</button>
+    </div>
+  )
+}
+
+export default Resources
